53abba1e4d0391eea20a0582352b5f858ef00e13
rajeshjaiswal878 <rajeshjaiswal878@gmail.com>
2016-08-31 21:57:52 -0500

    This is 1st Assignment of Data Science Essenstials  Hello World Assignement  Rajesh Jaiswal Dated 08/31/2016

---
+++ Hello_World_Assign1.py
@@ -0,0 +1,14 @@
+'''
+----------------------------------------Creator's Details----------------------------
+Creator: RAJESH A.JAISWAL
+UIN:669012329
+DEPARTMENT: COMPUTER SCIENCE  MS FALL-2016
+DATE: 08/31/2016
+ASSIGNMENT1: HELLO WORLD IN py
+--------------------------------------------------------------------------------------
+'''
+
+print "Hello world"
+print "UIS CSC R570 Data Science Essential 1st Assignement"
+print "Thank you for checking this"
+exit()
